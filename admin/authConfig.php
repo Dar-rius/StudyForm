9e502cce257be840a8b717709a8fc9ad904f0acb
Dar-rius <mohamedtine17@gmail.com>
2022-05-11 23:02:39 +0000

    update in last file and add admin's folder with almost files

---
+++ admin/authConfig.php
@@ -0,0 +1,24 @@
+<?php
+include '../config.php';
+
+session_start();
+
+$query = "SELECT * FROM admins WHERE adminName = :adminName AND password = :password";  
+$statement = $bd->prepare($query);  
+$statement->execute(  
+        array(  
+            'adminName'     =>     $_POST["adminName"],  
+            'password'     =>     $_POST["password"]  
+        )  
+);  
+$count = $statement->rowCount();  
+if($count > 0)  
+{  
+        $_SESSION["adminName"] = $_POST["adminName"];  
+        header("location: ../read.php");  
+}  
+else  
+{  
+        echo '<label>Wrong Data</label>';  
+}  
+   
