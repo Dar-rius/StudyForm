019df9dce775fe1163ed2b44da1e91dab7d69d9c
Dar-rius <mohamedtine17@gmail.com>
2022-05-11 23:04:44 +0000

    fixed error

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
     try {
-    	$bd = new PDO('mysql:host=localhost;dbname=id18917055_forum	;charset=utf8', 'id18917055_root', '=#z5{p+&nJ8TG%px');
+    	$bd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8', 'root', '');
     } catch (PDOException $exception) {
     	// If there is an error with the connection, stop the script and display the error.
     	exit('Failed to connect to database!');
